3f5aa94aa6b7735f998521af73006f387231f5fb
Chinmay <chinmay_dd@yahoo.co.in>
2014-05-25 22:42:44 +0530

    Final Git Practice Subtraction and Cosine

---
+++ edit2.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+void trig_cos()
+{
+char ch='y';
+while(ch=='y')
+{
+  float n;
+  cout<<"\n Please enter the number(in radians) whose cosine you wish to calculate : ";
+  cin>>n;
+  cout<<"\n The cosine of the above number is "<<cos(n)<<"\n";
+  cout<<"\n Do you wish to continue? Yes->y No->n";
+  cin>>ch;
+}
+return;
+}  
+
+void subtract()
+{
+char ch='y';
+while(ch=='y')
+{
+  float a,b;
+  cout<<"\n Please enter the numbers you wish to subtract : ";
+  cout<<"\n a = ";
+  cin>>a;
+  cout<<"\n b = ";
+  cin>>b;
+  cout<<"The answer is "<<a-b<<" \n";
+  cout<<"\n Do you wish to continue? Yes->y No->n";
+  cin>>ch;
+}
+return;
+}
+
+int main()
+{
+  cout<<"Welcome to the pro calculator. Choose from foll. :\n";
+  cout<<"1.Add\n";
+  cout<<"2.Subtract\n";
+  cout<<"3.Multiply\n";
+  cout<<"4.Divide\n";
+  cout<<"5.Exponentiation\n";
+  cout<<"6.cos\n"
+  cout<<"7.sin\n";
+  cout<<"8.tan\n";
+
+  int ch;
+  cin>>ch;
+
+  switch(ch)
+  {
+	case 1 : add();break;
+	case 2 : subtract();break;
+	case 3 : multiply();break;
+	case 4 : divide();break;
+	case 5 : exponentiation();break;	
+	case 6 : trig_cos();break;
+	case 7 : trig_sin();break;
+	case 8 : trig_tan();break;
+  }
+
+  return 0;
+
+}
